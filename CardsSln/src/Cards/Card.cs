6a2f0a6b607c70fd12878bfd9acd51df94b29b51
jhedges-wr <34311306+jhedges-wr@users.noreply.github.com>
2019-06-06 07:39:25 -0600

    Adding changed files

--- CardsSln/src/Cards/Card.cs
+++ CardsSln/src/Cards/Card.cs
@@ -2,60 +2,34 @@ using System.Runtime.Serialization;
 
 namespace Cards
 {
-    public class Card
+    public interface ICard
+    {
+        public Suit Suit { get; set; }
+        public int Rank { get; set; }
+    }
+
+    public enum Suit
+    {
+        [EnumMember(Value = "Spade")]
+        Spade,
+        [EnumMember(Value = "Club")]
+        Club,
+        [EnumMember(Value = "Diamond")]
+        Diamond,
+        [EnumMember(Value="Heart")]
+        Heart
+    }
+
+    public class Card : ICard
     {
         public Card()
         {
             
         }
 
-        public enum Suit
-        {
-            [EnumMember(Value = "Spade")]
-            Spade,
-            [EnumMember(Value = "Club")]
-            Club,
-            [EnumMember(Value = "Diamond")]
-            Diamond,
-            [EnumMember(Value="Heart")]
-            Heart
-        }
 
         public Suit Suit { get; set; }
         public int Rank { get; set; }
-    }
-
-    public class AceCard : Card
-    {
-        public Ace()
-        {
-            AceHigh = true;
-        }
-
-        public int Rank 
-        { 
-            get
-            {
-                return AceHigh ? 14 : 1;
-            }
-            set
-            {
-                if (value == 1)
-                {
-                    AceHigh = false;
-                }
-                else if (value == 14)
-                {
-                    AceHigh = true;
-                }
-                else 
-                {
-                    throw new ArgumentException("Invalid rank for an Ace card");
-                }
-            }
-        }
-
-        public bool AceHigh {get; set;}
     }
 
 }
